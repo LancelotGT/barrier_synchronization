9e2fb8ee27403001bbc468ecf4fc6a875d288a5d
Ning Wang <glningwang@gmail.com>
2015-10-14 18:29:50 +0000

    place localsense in separate cacheline for optimization

--- OpenMP/gtmp_counter.c
+++ OpenMP/gtmp_counter.c
@@ -21,31 +21,37 @@
 static int P;
 static int count;
 static int sense;
-static int* localSense;
+static int** localSense;
 
 void gtmp_init(int num_threads){
     P = num_threads;
     count = num_threads;
     sense = 1;
-    localSense = malloc(num_threads * sizeof(int));
+    localSense = malloc(num_threads * sizeof(int*));
+
     int i;
     for (i = 0; i < num_threads; i++)
-        localSense[i] = 1;
+        posix_memalign((void*) &localSense[i], LEVEL1_DCACHE_LINESIZE, sizeof(int)); 
+    for (i = 0; i < num_threads; i++)
+        *localSense[i] = 1;
 }
 
 void gtmp_barrier(){
     int thread_num = omp_get_thread_num();
-    localSense[thread_num] = !localSense[thread_num];
+    *localSense[thread_num] = !(*localSense[thread_num]);
 
     if (__sync_fetch_and_sub(&count, 1) == 1)
     {   /* if it is the last processor */ 
         count = P;
-        sense = localSense[thread_num];
+        sense = *localSense[thread_num];
     }
     else
-        while (sense != localSense[thread_num]);
+        while (sense != *localSense[thread_num]);
 }
 
 void gtmp_finalize(){
+    int i = 0;
+    for (i = 0; i < P; i++)
+        free(localSense[i]);
     free(localSense);
 }
