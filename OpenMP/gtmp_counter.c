cc00ae1457141aba9d3d975266818b9f496495c4
Ning Wang <glningwang@gmail.com>
2015-10-14 17:57:26 +0000

    add sense reverse barrier for OpenMP

---
+++ OpenMP/gtmp_counter.c
@@ -0,0 +1,51 @@
+#include <omp.h>
+#include "gtmp.h"
+#include <stdlib.h>
+
+/*
+    From the MCS Paper: A sense-reversing centralized barrier
+
+    shared count : integer := P
+    shared sense : Boolean := true
+    processor private local_sense : Boolean := true
+
+    procedure central_barrier
+        local_sense := not local_sense // each processor toggles its own sense
+	if fetch_and_decrement (&count) = 1
+	    count := P
+	    sense := local_sense // last processor toggles global sense
+        else
+           repeat until sense = local_sense
+*/
+
+static int P;
+static int count;
+static int sense;
+static int* localSense;
+
+void gtmp_init(int num_threads){
+    P = num_threads;
+    count = num_threads;
+    sense = 1;
+    localSense = malloc(num_threads * sizeof(int));
+    int i;
+    for (i = 0; i < num_threads; i++)
+        localSense[i] = 1;
+}
+
+void gtmp_barrier(){
+    int thread_num = omp_get_thread_num();
+    localSense[thread_num] = !localSense[thread_num];
+
+    if (__sync_fetch_and_sub(&count, 1) == 1)
+    {   /* if it is the last processor */ 
+        count = P;
+        sense = localSense[thread_num];
+    }
+    else
+        while (sense != localSense[thread_num]);
+}
+
+void gtmp_finalize(){
+    free(localSense);
+}
--- OpenMP/hello_world.c
+++ OpenMP/hello_world.c
@@ -1,5 +1,6 @@
 #include <omp.h>
 #include <stdio.h>
+#include "gtmp.h"
 
 #define NUM_THREADS 3
 
@@ -9,16 +10,19 @@ int main(int argc, char **argv)
   // Serial code
   printf("This is the serial section\n");
   omp_set_num_threads(NUM_THREADS);
+  gtmp_init(NUM_THREADS);
 
 #pragma omp parallel
   {
     // Now we're in the parallel section
     int num_threads = omp_get_num_threads();
     int thread_num = omp_get_thread_num();
+    gtmp_barrier(); 
     printf("Hello World from thread %d of %d.\n", thread_num, num_threads);
-  } // implied barrier
+  } 
 
   // Resume serial code
+  gtmp_finalize();
   printf("Back in the serial section again\n");
   return 0;
 }
